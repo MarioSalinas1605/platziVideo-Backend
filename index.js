76df0bce9b0b7fe4addc04a69e512ba3f065b4b3
Mario Salinas <mario_1605@live.com.mx>
2020-07-04 11:11:37 -0500

    feat: not found validation with boom

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 
 const config = require('./config/index');
 const moviesApi = require('./routes/movies');
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 app.use(express.json());
 
 moviesApi(app);
 
+app.use(notFoundHandler);
+
 app.use(logErrors);
-app.use(errorHandler)
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`);
