e0ba6151cd4fd3e4d961b477b884dc05df7d8be6
Mario Salinas <mario_1605@live.com.mx>
2020-07-13 07:03:49 -0500

    feat: basic strategy authentication

---
+++ utils/auth/strategies/basic.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+
+const UsersService = require('../../../services/users');
+
+passport.use(new BasicStrategy(async (email, password, cb) => {
+    const userService = new UsersService();
+
+    try {
+        const user = await userService.getUser({ email });
+        if (!user) {
+            return(boom.unauthorized(), false);
+        }
+        if (!(await bcrypt.compare(password, user.password))) {
+            return(boom.unauthorized(), false);
+        }
+        delete user.password;
+    } catch (error) {
+        return cb(error);
+    }
+}))
